33e85772174d19cad71473daa0793eb7d0b91374
Bo Henriksen <stumpdenmark@hotmail.com>
2020-07-10 00:30:29 +0200

    Update set-urls.sh

--- set-urls.sh
+++ set-urls.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-if ["$TRAVIS_BRANCH" = "development" ]; then 
-    sed -i -e 's/{WORDPRESS_URL}/https:\/\/beta.link-lives.dk/' Dockerfile 
+if [[ "$TRAVIS_BRANCH" = "development" ]]; then 
+    sed -i -e 's/{WORDPRESS_URL}/https:\/\/beta.link-lives.dk/g' Dockerfile1
+fi
+if [[ "$TRAVIS_BRANCH" = "master" ]]; then 
+    sed -i -e 's/{WORDPRESS_URL}/https:\/\/link-lives.dk/g' Dockerfile1 
 fi
